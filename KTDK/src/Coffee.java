978ed85865b8e3d68aa3d4109ecf76bf6889469a
Admin <phucla20.cdsg@saigontech.edu.vn>
2022-05-24 17:46:09 +0700

    Initial commit

---
+++ KTDK/src/Coffee.java
@@ -0,0 +1,18 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.event.ActionEvent;
+public class Coffee{
+
+    @FXML
+    private TextArea textarea;
+
+    @FXML
+    private Button coffee;
+
+    @FXML
+    void onPress(ActionEvent event) {
+
+    }
+
+}
